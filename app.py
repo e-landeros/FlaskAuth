9d845dc987ae9e9962a67f518e7d7dd86fe92f41
e-landeros <landerosedgard@gmail.com>
2018-12-05 19:51:56 -0800

    base template created

---
+++ app.py
@@ -0,0 +1,10 @@
+from myproject import app,db
+from flask import render_template,redirect,request, url_for, flash, abort
+from flask_login import login_user, login_required,logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+
+
+@app.route('/')
+def func_name(foo):
+    return render_template('home.html')
