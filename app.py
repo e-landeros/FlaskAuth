dbecd6091dd5e663863584d6994e724f5abda528
e-landeros <landerosedgard@gmail.com>
2018-12-05 20:53:22 -0800

    login views

--- app.py
+++ app.py
@@ -6,5 +6,54 @@ from myproject.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
-def func_name(foo):
+def home():
     return render_template('home.html')
+
+@app.route('/welcome')
+@login_required   #this decorator is used to redirect to login in page instead fo welcome_user.html
+def welcome_user():
+    return render_template('welcome_user.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You logged out!")
+    return redirect(url_for('home'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Logged in Successfully!')
+
+            next = request.args.get('next') # gets next page from previous request
+
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+
+        return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+   form = RegistrationForm()
+
+   if form.validate_on_submit():
+       user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Thanks for registering.")
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form )
+
+if __name__ = '__main__':
+    app.run(debug=True)
